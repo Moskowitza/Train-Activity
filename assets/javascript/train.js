bdfa39ab1f573c57ac1b8b1936045050d680c2a2
moskowitza <moskowitzaaron@mac.com>
2018-03-19 16:02:05 -0400

    works but the time is a date

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,31 +1,21 @@
-// Link to our Firebase: This is right!
+// Link to our Firebase: This is working
 var config = {
-    apiKey: "AIzaSyBFItteSwAii_eYcCtGk2tITKwpUGMQqbw",
-    authDomain: "trains-230d3.firebaseapp.com",
-    databaseURL: "https://trains-230d3.firebaseio.com",
-    projectId: "trains-230d3",
-    storageBucket: "trains-230d3.appspot.com",
-    messagingSenderId: "362734551936"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBFItteSwAii_eYcCtGk2tITKwpUGMQqbw",
+  authDomain: "trains-230d3.firebaseapp.com",
+  databaseURL: "https://trains-230d3.firebaseio.com",
+  projectId: "trains-230d3",
+  storageBucket: "trains-230d3.appspot.com",
+  messagingSenderId: "362734551936"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
-//declare variables
-var name = "Train Name Alpha"; //send to firebase
-var destination = "Destination Alpha"; //send to firebase
-var firstTrain =  "03:30"; //send to firebase
-var firstTimeFormat="hh:mm" //CHECK ON THIS = for momentjs
-var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-console.log(firstTimeConverted);
-var frequency = 10; //send to firebase
-var frequencyFormat = "minutes"; // //CHECK ON THIS = for momentjs
-var nextArrival= "00:00" ; //CHECK ON THIS =send to the DOM
-var minutesAway= "00:00"; //CHECK ON THIS =send to the DOM
-var currentTime=moment(); //for calculations
+//declare variables being submitted to the database
+var name = "Septa"; //send to firebase
+var destination = "30th Street Station"; //send to firebase
+var firstTime = "03:30"; //send to firebase
+var tFrequency = 3; //send to firebase 
 
-// Difference between the times
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
 
 //Submit button, send data to Firebase
 $("#submit").on("click", function (event) {
@@ -34,14 +24,15 @@ $("#submit").on("click", function (event) {
   //trim and validate data here, before it's sent to firebase
   name = $("#name").val().trim();
   destination = $("#destination").val().trim();
-  fristTrain = $("#firstTrain").val().trim();
-  frequency = $("#frequency").val().trim();
+  firstTime = $("#firstTime").val().trim();
+  tFrequency = $("#tFrequency").val();
 
+  //use PUSH instead of SET to keep values of trains in firebase
   database.ref().push({
-    name = name,
-    destination = destination,
-    fristTrain = firstTrain,
-    frequency = frequency
+    name: name,
+    destination: destination,
+    firstTime: firstTime,
+    tFrequency: tFrequency
   });
 });
 
@@ -50,23 +41,38 @@ $("#submit").on("click", function (event) {
 database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (childsnapshot) {
 
   var tRow = $("<tr>");
-  var tBody = $("tbody");
-  // tBody.empty(); //remove this to get list of all new values
-  // Calculate Next Arrival and Minues Away
-  //nex arrival = lasttraintime-currenttime
-  var startDate = childsnapshot.val().startDate;
-  console.log(startDate); //does get last date, but as YYYY/MM/DD 
-  var startMoment = moment(startDate).format("MM/DD/YYYY");
-  console.log(startMoment);//this is an object
-  var monthsDiffernce = moment().diff(moment(startMoment), "M"); //should be a number but isn't
-  var monthsNumber = parseInt(monthsDiffernce);
-  console.log(monthsNumber); //this is a negative number
-  //calculate TOTAL
-  var rate = childsnapshot.val().monthlyRate;
-  console.log(rate); //this is a string
-  var rateNumber = parseInt(rate);
-  console.log(rateNumber); //a number!
-  var total = monthsNumber * rateNumber;
+  var tBody = $("#schedule");
+
+    // 
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTime=childsnapshot.val().firstTime; 
+    var timeFormat = "HH:mm"
+    var firstTimeConverted = moment(firstTime, timeFormat).subtract(1, "Day"); //This is "date math"
+    console.log(firstTimeConverted);
+
+    var tFrequencyFormat = "minutes"; // //CHECK ON THIS = for momentjs
+
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format(timeFormat));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "hh:mm");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  
 
 
   //Check these names. Use childsnapshot.val() if pulling from database, otherwise put calculated
@@ -74,10 +80,12 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
 
   var nameTd = $("<td>").text(childsnapshot.val().name);//from database
   var destinationTd = $("<td>").text(childsnapshot.val().destination);//from database
-  var frequencyTd = $("<td>").text(childsnapshot.val().frequency);//from database
-  var nextArrivalTd = $("<td>").text(monthsDiffernce); //calculated value
-  var minutesAwayTd = $("<td>").text("$" + childsnapshot.val().monthlyRate); //from database
-  tRow.append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd);
+  var tFrequencyTd = $("<td>").text(childsnapshot.val().tFrequency);//from database
+
+
+  var nextTrainTd = $("<td>").text(nextTrain); //calculated above
+  var tMinutesTillTrainTd = $("<td>").text(tMinutesTillTrain); //calculated above
+  tRow.append(nameTd, destinationTd, tFrequencyTd, nextTrainTd, tMinutesTillTrainTd);
   console.log(tRow);
   tBody.append(tRow);
 })
